e88c12e30d804e86ba43b3e4e4aa14768078f4bf
Ianlent <invisiblefullmoon2@gmail.com>
2025-06-03 17:21:59 +0700

    Trimmed auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ import express from "express";
 import { login } from "../controller/authController.js";
 import { loginValidation } from "../middleware/validators/authValidator.js";
 import { handleValidationErrors } from "../middleware/handleValidationErrors.js";
-import authenticateToken from "../middleware/auth/authenticateToken.js";
 
 const router = express.Router();
 
